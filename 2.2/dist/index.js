583d3b4fbee94b941ef0059c5a1138327b337f34
OlhaHluboka <136605298+OlhaHluboka@users.noreply.github.com>
2023-09-25 23:42:17 +0300

    ++3-1

--- 2.2/dist/index.js
+++ 2.2/dist/index.js
@@ -1,6 +1,49 @@
 "use strict";
-/* Task 1 */
+/*   TASK   1 */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const url = "https://api.ipify.org/?format=json";
-const ip1 = fetch(url).then(data => data.json()).then(data => console.log("111  " + data.ip)).catch(e => console.log("fail"));
-console.log("8888 " + ip1);
+/* const ip1 = fetch(url)
+    .then(data => data.json())
+    .then(data => console.log("Promise is done: " + data.ip))
+    .catch(e => console.log("fail"));
+
+console.log("Waiting Process of Promise " + ip1); */
+const ip2 = (params) => __awaiter(void 0, void 0, void 0, function* () {
+    let request = yield fetch(params);
+    let data = yield request.json();
+    console.log("Task 1: " + data.ip);
+});
+ip2(url);
+/*   TASK 2   */
+function getMyIP(params) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let myPromise = yield fetch(params); // receiving data from server
+        let myId = yield myPromise.json().then(myPromise => { return myPromise.ip; }); // translate received data to json
+        console.log("Task 2: " + myId);
+        return myId;
+    });
+}
+getMyIP(url);
+/*   TASK 3   */
+const url1 = "https://random-data-api.com/api/name/random_name";
+function getNames1(params) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let firstPromise = (yield fetch(params)).json();
+        let secondPromise = (yield fetch(params)).json();
+        let thirdPromise = (yield fetch(params)).json();
+        let response = yield Promise.all([firstPromise, secondPromise, thirdPromise]);
+        let names = response.map((n) => " " + n.name).toString();
+        console.log("Task 3:" + names);
+        return names;
+    });
+}
+getNames1(url1);
 // npm run build && npm run start
--- 2.2/src/index.ts
+++ 2.2/src/index.ts
@@ -1,11 +1,62 @@
-/* Task 1 */
+/*   TASK   1 */
 
-const url:string = "https://api.ipify.org/?format=json";
+const url: string = "https://api.ipify.org/?format=json";
 
-const ip1 = fetch(url).then(data => data.json()).then(data => console.log("Promise is done: " + data.ip)).catch(e => console.log("fail"));
+/* const ip1 = fetch(url)
+    .then(data => data.json())
+    .then(data => console.log("Promise is done: " + data.ip))
+    .catch(e => console.log("fail"));
 
-console.log("Waiting Process of Promise "+ ip1);
+console.log("Waiting Process of Promise " + ip1); */
 
-  
+const ip2 = async (params: string) => {
+    
+    let request = await fetch(params);
+    let data = await request.json();
+    console.log("Task 1: " + data.ip)
+}
 
-  // npm run build && npm run start
+ip2(url);
+
+
+/*   TASK 2   */
+
+async function getMyIP(params: string) {
+
+    let myPromise = await fetch(params); // receiving data from server
+
+    let myId = await myPromise.json().then(myPromise => { return myPromise.ip }) // translate received data to json
+
+    console.log("Task 2: " + myId);
+
+    return myId;
+}
+
+getMyIP(url);
+
+
+/*   TASK 3   */
+
+const url1 = "https://random-data-api.com/api/name/random_name";
+
+async function getNames1(params:string) {
+
+    let firstPromise = (await fetch(params)).json();
+    let secondPromise = (await fetch(params)).json();
+    let thirdPromise = (await fetch(params)).json();
+
+    let response = await Promise.all([firstPromise, secondPromise, thirdPromise])
+
+    let names = response.map((n) => " " + n.name).toString();
+
+    console.log("Task 3:" + names);
+
+    return names;
+    
+}
+
+getNames1(url1);
+
+
+
+// npm run build && npm run start
