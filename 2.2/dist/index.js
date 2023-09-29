68a33ea3e2ff8e6dbbd3d7fb5fd26586627706a5
OlhaHluboka <136605298+OlhaHluboka@users.noreply.github.com>
2023-09-29 18:35:03 +0300

    +3-c

--- 2.2/dist/index.js
+++ 2.2/dist/index.js
@@ -1,5 +1,3 @@
-"use strict";
-/*   TASK   1 */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,6 +7,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+/*   TASK   1 */
+import fetch from "node-fetch";
 const url = "https://api.ipify.org/?format=json";
 /* const ip1 = fetch(url)
     .then(data => data.json())
@@ -25,25 +25,75 @@ ip2(url);
 /*   TASK 2   */
 function getMyIP(params) {
     return __awaiter(this, void 0, void 0, function* () {
-        let myPromise = yield fetch(params); // receiving data from server
-        let myId = yield myPromise.json().then(myPromise => { return myPromise.ip; }); // translate received data to json
+        let data = yield fetch(params); // receiving data from server
+        let myId = yield data.json().then(myObject => { return myObject.ip; }); // translate received data to json
         console.log("Task 2: " + myId);
         return myId;
     });
 }
 getMyIP(url);
 /*   TASK 3   */
+// With Promise.all
 const url1 = "https://random-data-api.com/api/name/random_name";
 function getNames1(params) {
     return __awaiter(this, void 0, void 0, function* () {
-        let firstPromise = (yield fetch(params)).json();
-        let secondPromise = (yield fetch(params)).json();
-        let thirdPromise = (yield fetch(params)).json();
+        let firstPromise = (yield fetch(params)).json().catch(console.log.bind(console));
+        let secondPromise = (yield fetch(params)).json().catch(console.log.bind(console));
+        let thirdPromise = (yield fetch(params)).json().catch(console.log.bind(console));
         let response = yield Promise.all([firstPromise, secondPromise, thirdPromise]);
         let names = response.map((n) => " " + n.name).toString();
-        console.log("Task 3:" + names);
+        console.log("Task 3-a:" + names);
         return names;
     });
 }
 getNames1(url1);
+// Without Promise.all
+const urls = [
+    "https://random-data-api.com/api/name/random_name",
+    "https://random-data-api.com/api/name/random_name",
+    "https://random-data-api.com/api/name/random_name"
+];
+function getNames2(params) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let responses = [];
+        try {
+            for (let request of params) {
+                let result = yield fetch(request);
+                let myjson = yield result.json();
+                responses.push(myjson.name);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+        console.log("Task 3-b: " + responses.toString());
+        return responses.toString;
+    });
+}
+getNames2(urls);
+// Without Promise.all and without async/await: polifil for Promise.All
+function getArrayOfAllPromises(params) {
+    let prms = [];
+    params.forEach((ip) => {
+        let one = fetch(ip).then(str => { return str.json(); }).then(nm => { return nm.name; }).catch(e => { console.log(e); });
+        prms.push(one);
+    });
+    return prms;
+}
+function getPromises(promises) {
+    const arrayOfStrings = [];
+    let promise = new Promise((resolve, reject) => {
+        promises.forEach((prm) => {
+            prm.then((str) => {
+                arrayOfStrings.push(str);
+                if (arrayOfStrings.length === promises.length) {
+                    resolve(arrayOfStrings);
+                }
+            });
+        });
+    });
+    promise.then(names => console.log("Task 3-c: " + names));
+    return promise;
+}
+getPromises(getArrayOfAllPromises(urls));
 // npm run build && npm run start
--- 2.2/src/index.ts
+++ 2.2/src/index.ts
@@ -1,4 +1,6 @@
 /*   TASK   1 */
+import fetch from "node-fetch";
+
 
 const url: string = "https://api.ipify.org/?format=json";
 
@@ -10,7 +12,7 @@ const url: string = "https://api.ipify.org/?format=json";
 console.log("Waiting Process of Promise " + ip1); */
 
 const ip2 = async (params: string) => {
-    
+
     let request = await fetch(params);
     let data = await request.json();
     console.log("Task 1: " + data.ip)
@@ -23,9 +25,9 @@ ip2(url);
 
 async function getMyIP(params: string) {
 
-    let myPromise = await fetch(params); // receiving data from server
+    let data = await fetch(params); // receiving data from server
 
-    let myId = await myPromise.json().then(myPromise => { return myPromise.ip }) // translate received data to json
+    let myId = await data.json().then(myObject => { return myObject.ip }) // translate received data to json
 
     console.log("Task 2: " + myId);
 
@@ -37,26 +39,94 @@ getMyIP(url);
 
 /*   TASK 3   */
 
+// With Promise.all
+
 const url1 = "https://random-data-api.com/api/name/random_name";
 
-async function getNames1(params:string) {
+async function getNames1(params: string) {
 
-    let firstPromise = (await fetch(params)).json();
-    let secondPromise = (await fetch(params)).json();
-    let thirdPromise = (await fetch(params)).json();
+    let firstPromise = (await fetch(params)).json().catch(console.log.bind(console));
+    let secondPromise = (await fetch(params)).json().catch(console.log.bind(console));
+    let thirdPromise = (await fetch(params)).json().catch(console.log.bind(console));
 
     let response = await Promise.all([firstPromise, secondPromise, thirdPromise])
 
     let names = response.map((n) => " " + n.name).toString();
 
-    console.log("Task 3:" + names);
+    console.log("Task 3-a:" + names);
 
     return names;
-    
+
 }
 
 getNames1(url1);
 
+// Without Promise.all
+const urls = [
+    "https://random-data-api.com/api/name/random_name",
+    "https://random-data-api.com/api/name/random_name",
+    "https://random-data-api.com/api/name/random_name"];
 
+async function getNames2(params: string[]) {
+
+    let responses = [];
+
+    try {
+        for (let request of params) {
+            let result = await fetch(request);
+            let myjson = await result.json();
+            responses.push(myjson.name);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    console.log("Task 3-b: " + responses.toString())
+
+    return responses.toString;
+}
+
+getNames2(urls);
+
+// Without Promise.all and without async/await: polifil for Promise.All
+
+function getArrayOfAllPromises(params: string[]) {
+
+    let prms: any = [];
+
+        params.forEach((ip) => {
+            let one = fetch(ip).then(str => { return str.json() }).then(nm => { return nm.name }).catch(e => { console.log(e) });
+            prms.push(one)
+        })
+    
+    return prms;
+}
+
+function getPromises(promises:any) {
+    
+    const arrayOfStrings:string[] = [];
+
+    
+    let promise = new Promise((resolve, reject) => {
+
+        promises.forEach((prm:any) => {
+            prm.then((str:string) => {
+
+                arrayOfStrings.push(str);
+
+                if (arrayOfStrings.length === promises.length) {
+                    resolve(arrayOfStrings);
+                }
+            })
+        })
+    })
+
+    promise.then(names => console.log("Task 3-c: " + names));
+
+    return promise;
+}
+
+getPromises(getArrayOfAllPromises(urls));
 
 // npm run build && npm run start
