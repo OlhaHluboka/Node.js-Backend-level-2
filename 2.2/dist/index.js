dde2c76e84212ea96554c47f60f4a40221a2ebda
OlhaHluboka <136605298+OlhaHluboka@users.noreply.github.com>
2023-10-03 21:26:20 +0300

    ++6

--- 2.2/dist/index.js
+++ 2.2/dist/index.js
@@ -33,7 +33,7 @@ function getMyIP(params) {
 }
 getMyIP(url);
 /*   TASK 3   */
-// With Promise.all
+// a: With Promise.all
 const url1 = "https://random-data-api.com/api/name/random_name";
 function getNames1(params) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -47,7 +47,7 @@ function getNames1(params) {
     });
 }
 getNames1(url1);
-// Without Promise.all
+// b: Without Promise.all
 const urls = [
     "https://random-data-api.com/api/name/random_name",
     "https://random-data-api.com/api/name/random_name",
@@ -71,7 +71,7 @@ function getNames2(params) {
     });
 }
 getNames2(urls);
-// Without Promise.all and without async/await: polifil for Promise.All
+// c: Without Promise.all and without async/await: polifil for Promise.All
 function getArrayOfAllPromises(params) {
     let prms = []; // for array of promises
     params.forEach((ip) => {
@@ -96,4 +96,105 @@ function getPromises(promises) {
     return promise;
 }
 getPromises(getArrayOfAllPromises(urls));
+/*   TASK 4   */
+// a: Without async/await
+const url2 = "https://random-data-api.com/api/users/random_user";
+let count1 = 0;
+let count2 = 0;
+/**
+ * This function creates and returns a promise, which will fulfill when come a request
+ * from server we appeal.
+ * @param address - a string with url of server.
+ * @returns - a promise resolved an information from server.
+ */
+function getGender(address) {
+    let promise = new Promise((resolve, reject) => {
+        fetch(address)
+            .then(receivedData => { return receivedData.json(); })
+            .then(receivedString => { resolve(receivedString.gender); })
+            .catch(error => reject(console.log(error)));
+    });
+    return promise;
+}
+/**
+ * Contains a recursive request to server for obtaining needed information about gender "Female".
+ * @param gender - result of fulfilling a promise to fetch an info from server;
+ * @returns - string with count of trying to get femail and gender "Female";
+ */
+function getFemale(gender) {
+    return gender.then(gnd => {
+        count1++;
+        if (gnd === "Female") {
+            console.log(`Task 4-a: ${count1} requests need to obtain a gender ` + gnd + ' from server.');
+        }
+        else {
+            getFemale(getGender(url2));
+        }
+    }).catch(e => console.log(e));
+}
+getFemale(getGender(url2));
+// b: With async/await
+/**
+ * This is an asyncronic function which returns a fulfilled promise.
+ * With world "await" JS waiting for the promise fulfills.
+ * Promise is a fetch to server by url and get string data about a gender.
+ * @param server - url of server;
+ * @returns - fulfilled promise.
+ */
+function getResponse(server) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield fetch(server).then(str => { return str.json(); }).then(gndr => { return gndr.gender; }).catch(e => (console.log(e)));
+    });
+}
+/**
+ * Recursive function which seeks a femsle gender from responses by server we apeal.
+ * @param gender1 - fulfilled promise with result is a gender one.
+ * @returns - a string with female gender or recursive apeaal to server.
+ */
+function getResult(gender1) {
+    return __awaiter(this, void 0, void 0, function* () {
+        count2++;
+        return gender1.then(gnd => {
+            if (gnd === "Female") {
+                console.log(`Task 4-b: Need to pass ${count2} times to fetch a gender ` + gnd);
+            }
+            else {
+                getResult(getResponse(url2));
+            }
+        });
+    });
+}
+getResult(getResponse(url2));
+/*   TASK 5   */
+function function2GetMyIP(ip) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return fetch(ip).then(str => { return str.json(); }).then(myIp => { return myIp.ip; }).catch(e => (console.log(e)));
+    });
+}
+function function1(callback) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let myIp = yield function2GetMyIP(url);
+        callback(myIp);
+    });
+}
+function callback(param) {
+    console.log("Task 5: " + param);
+}
+function1(callback);
+/*   TASK 6   */
+function func1GetMyIp(myUrl) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return fetch(myUrl).then(str => { return str.json(); }).then(myIp => { return myIp.ip; }).catch(e => (console.log(e)));
+    });
+}
+function func2(callback1) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let myIp = yield func1GetMyIp(url);
+        callback1(myIp);
+    });
+}
+function callback1(param1) {
+    console.log("Task 6: " + param1);
+}
+func2(callback1);
 // npm run build && npm run start
--- 2.2/src/index.ts
+++ 2.2/src/index.ts
@@ -39,7 +39,7 @@ getMyIP(url);
 
 /*   TASK 3   */
 
-// With Promise.all
+// a: With Promise.all
 
 const url1 = "https://random-data-api.com/api/name/random_name";
 
@@ -61,7 +61,7 @@ async function getNames1(params: string) {
 
 getNames1(url1);
 
-// Without Promise.all
+// b: Without Promise.all
 const urls = [
     "https://random-data-api.com/api/name/random_name",
     "https://random-data-api.com/api/name/random_name",
@@ -89,28 +89,28 @@ async function getNames2(params: string[]) {
 
 getNames2(urls);
 
-// Without Promise.all and without async/await: polifil for Promise.All
+// c: Without Promise.all and without async/await: polifil for Promise.All
 
 function getArrayOfAllPromises(params: string[]) {
 
     let prms: Promise<string>[] = []; // for array of promises
 
-        params.forEach((ip) => {
-            let one = fetch(ip).then(str => { return str.json() }).then(nm => { return nm.name }).catch(e => { console.log(e) });
-            prms.push(one)
-        })
-    
+    params.forEach((ip) => {
+        let one = fetch(ip).then(str => { return str.json() }).then(nm => { return nm.name }).catch(e => { console.log(e) });
+        prms.push(one)
+    })
+
     return prms;
 }
 
-function getPromises(promises:Promise<string>[]) {
-    
-    const arrayOfStrings:string[] = [];
+function getPromises(promises: Promise<string>[]) {
+
+    const arrayOfStrings: string[] = [];
 
     let promise = new Promise((resolve, reject) => {
 
-        promises.forEach((prm:Promise<string>) => {
-            prm.then((str:string) => {
+        promises.forEach((prm: Promise<string>) => {
+            prm.then((str: string) => {
 
                 arrayOfStrings.push(str);
 
@@ -127,5 +127,135 @@ function getPromises(promises:Promise<string>[]) {
 }
 
 getPromises(getArrayOfAllPromises(urls));
+
+/*   TASK 4   */
+
+// a: Without async/await
+
+const url2 = "https://random-data-api.com/api/users/random_user";
+
+let count1: number = 0;
+let count2: number = 0;
+
+/**
+ * This function creates and returns a promise, which will fulfill when come a request 
+ * from server we appeal.
+ * @param address - a string with url of server.
+ * @returns - a promise resolved an information from server.
+ */
+function getGender(address: string) {
+
+    let promise = new Promise((resolve, reject) => {
+
+        fetch(address)
+            .then(receivedData => { return receivedData.json() })
+            .then(receivedString => { resolve(receivedString.gender) })
+            .catch(error => reject(console.log(error)));
+
+    });
+
+    return promise;
+}
+
+/**
+ * Contains a recursive request to server for obtaining needed information about gender "Female".
+ * @param gender - result of fulfilling a promise to fetch an info from server;
+ * @returns - string with count of trying to get femail and gender "Female";
+ */
+function getFemale(gender: Promise<unknown>) {
+
+    return gender.then(gnd => {
+        count1++;
+        if (gnd === "Female") {
+            console.log(`Task 4-a: ${count1} requests need to obtain a gender ` + gnd + ' from server.');
+
+        } else {
+            getFemale(getGender(url2));
+        }
+    }).catch(e => console.log(e));
+
+}
+
+getFemale(getGender(url2));
+
+// b: With async/await
+
+/**
+ * This is an asyncronic function which returns a fulfilled promise.
+ * With world "await" JS waiting for the promise fulfills.
+ * Promise is a fetch to server by url and get string data about a gender.
+ * @param server - url of server;
+ * @returns - fulfilled promise.
+ */
+async function getResponse(server: string) {
+
+    return await fetch(server).then(str => { return str.json() }).then(gndr => { return gndr.gender }).catch(e => (console.log(e)));
+
+}
+
+/**
+ * Recursive function which seeks a femsle gender from responses by server we apeal.
+ * @param gender1 - fulfilled promise with result is a gender one.
+ * @returns - a string with female gender or recursive apeaal to server.
+ */
+async function getResult(gender1: Promise<string>) {
+    
+    count2++;
+    return gender1.then(gnd => {
+
+        if (gnd === "Female") {
+            console.log(`Task 4-b: Need to pass ${count2} times to fetch a gender `+gnd);
+        } else{
+            getResult(getResponse(url2));
+        }
+    })
+}
+
+getResult(getResponse(url2));
+
+
+/*   TASK 5   */
+
+async function function2GetMyIP(ip:string) {
+    return fetch(ip).then(str => { return str.json() }).then(myIp => { return myIp.ip }).catch(e => (console.log(e)));
+    
+}
+
+async function function1(callback:Function ) {
+    
+    let myIp = await function2GetMyIP(url);
+
+     callback(myIp);
+    
+}
+
+function callback(param:string) {
+    console.log("Task 5: " + param);
+}
+
+function1(callback);
+
+/*   TASK 6   */
+
+async function func1GetMyIp(myUrl:string) {
+
+    return fetch(myUrl).then(str => { return str.json() }).then(myIp => { return myIp.ip }).catch(e => (console.log(e)));
+   
+}
+
+async function func2(callback1:Function) {
+
+    let myIp = await func1GetMyIp(url);
+
+    callback1(myIp);
+    
+}
+
+function callback1(param1:string) {
+    console.log("Task 6: " + param1);
+}
+
+func2(callback1);
+
 
 // npm run build && npm run start
